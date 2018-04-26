5a132f549a6924323732a94c85f1cab84bad53c1
F嘉阳 <fjy8018@qq.com>
2018-04-26 19:37:46 +0800

    javadoc

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -5,13 +5,18 @@ var year = dt.getFullYear();
 var cur = year + '-' + month + '-' + day;
 
 function DateDiff(sDate1, sDate2) {    //sDate1和sDate2是2002-12-18格式
-    var aDate, oDate1, oDate2, iDays
-    aDate = sDate1.split("-")
-    oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])    //转换为12-18-2002格式
-    aDate = sDate2.split("-")
-    oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])
-    iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24)    //把相差的毫秒数转换为天数
-    return iDays
+    let aDate, oDate1, oDate2, iDays;
+    if (compareTime(sDate1, sDate2)) {
+        return "已截止";
+    }
+    else {
+        aDate = sDate1.split("-");
+        oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);    //转换为12-18-2002格式
+        aDate = sDate2.split("-");
+        oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]);
+        iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 / 24);    //把相差的毫秒数转换为天数
+        return iDays
+    }
 }
 
 function displayStyle(id, type) {
